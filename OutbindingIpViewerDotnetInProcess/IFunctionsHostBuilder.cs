53aff5dd16af0bb1505f69b25da90df8964a51d1
Carlos <carcarbe@gmail.com>
2021-07-30 06:17:07 +0200

    Add DI and add endpoint that returns outbinding ip

---
+++ OutbindingIpViewerDotnetInProcess/IFunctionsHostBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(OutbindingIpViewerDotnetInProcess.Startup))]
+namespace OutbindingIpViewerDotnetInProcess
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+
+            builder.Services.AddHttpClient("httpCLient", opts =>
+            {
+                opts.Timeout = TimeSpan.FromSeconds(600);
+            });
+        }
+    }
+}
--- OutbindingIpViewerDotnetInProcess/OutbindingIp.cs
+++ OutbindingIpViewerDotnetInProcess/OutbindingIp.cs
@@ -3,22 +3,33 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
 using System.Threading.Tasks;
+using static System.Environment;
 
 namespace OutbindingIpViewerDotnetInProcess
 {
-    public static class OutbindingIp
+    public class OutbindingIp
     {
+        private readonly HttpClient _httpClient;
+        public OutbindingIp(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
         [FunctionName("OutbindingIp")]
-        public static async Task<IActionResult> Run(
+        public  async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-         
+
+            var ipAddres = await _httpClient.GetAsync(GetEnvironmentVariable("requestIpUrl"));
+            return new OkObjectResult("Ipaddres:" + ipAddres);
+
+
             log.LogInformation("C# HTTP trigger function processed a request.");
             string result = req.HttpContext.Connection.RemoteIpAddress.ToString();
 
-           return new OkObjectResult(result);
+            return new OkObjectResult(result);
 
         }
     }
